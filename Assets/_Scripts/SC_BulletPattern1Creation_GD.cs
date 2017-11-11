25cf35e55cc3bc9256594a072f058fab8f822862
Unknown <bfdgab@hotmail.com>
2017-11-11 00:52:16 -0500

    BUllet modification
    Scrip changed of position
    New Script bullet life

---
+++ Assets/_Scripts/SC_BulletPattern1Creation_GD.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_BulletPattern1Creation_GD : MonoBehaviour {
+
+        public GameObject bulletPattern1Slot;
+        public bool bulletPattern1Alive = false;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 if (Input.GetKeyDown(KeyCode.Space)){
+            bulletPattern1Alive = true;         
+        }
+
+     if (bulletPattern1Alive == true)
+        {
+            Instantiate(bulletPattern1Slot);
+        }
+       if (Input.GetKeyUp(KeyCode.Space)){
+            bulletPattern1Alive = false;
+        }
+    }
+}
