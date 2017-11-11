8f6066eea3387de8ffef762b996599d34f1657f7
geekymoose <constantin.masson@umontreal.ca>
2017-11-11 13:38:17 -0800

    Add king's life

--- Assets/_Scripts/Game/SC_GameManager.cs
+++ Assets/_Scripts/Game/SC_GameManager.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class SC_GameManager : MonoBehaviour {
     public SC_BabyController babyController;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public SC_KingController kingController;
 	
 	// Update is called once per frame
 	void Update () {
 		if(!this.babyController.isAlive) {
             Debug.Log("GameOver: Baby just die!");
             // TODO: Game Over right Now!
+        }
+        else if(!this.kingController.isAlive) {
+            Debug.Log("GG Fucker!!");
+        }
+
+        if(this.babyController.isCrying) {
+            Debug.DrawLine(Vector3.zero, new Vector3(666,666,666), Color.red, 0.5f);
+            this.kingController.takeDamage(this.babyController.cryDamagePerSecond * Time.deltaTime);
         }
 	}
 }
---
+++ Assets/_Scripts/Game/SC_KingController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_KingController : MonoBehaviour {
+    public float maxHP;
+    public bool isAlive;
+    public float currentHP;
+
+	// Use this for initialization
+	void Start () {
+		this.currentHP = this.maxHP;
+        this.isAlive = true;
+	}
+
+    public void takeDamage(float dammageValue) {
+        this.currentHP -= dammageValue;
+        this.currentHP = Mathf.Clamp(this.currentHP, 0, this.maxHP);
+        if(this.currentHP <= 0f) {
+            this.isAlive = false;
+        }
+    }
+}
