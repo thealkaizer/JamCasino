51f0510e2036fe040750bf377f307a1eb38db812
Unknown <bfdgab@hotmail.com>
2017-11-10 23:01:00 -0500

    Floor and Bullet
    2Floor Prefab level
    1 bullet pattern

---
+++ Assets/_Prefabs/Bullet/SC_BulletPattern1Creation_GD.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_BulletPattern1Creation_GD : MonoBehaviour {
+
+        public GameObject bulletPattern1Slot;
+    public bool bulletPattern1Alive;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
