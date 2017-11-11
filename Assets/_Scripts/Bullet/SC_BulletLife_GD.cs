ad37cbcb54b90efa39ef54602bcaf49b2ba2c676
Unknown <bfdgab@hotmail.com>
2017-11-11 15:06:05 -0500

    Bullet pattern optimize boss death material change
    Bullet shape + destruction
    boss changing shape

--- Assets/_Scripts/Bullet/SC_BulletLife_GD.cs
+++ Assets/_Scripts/Bullet/SC_BulletLife_GD.cs
@@ -6,27 +6,31 @@ public class SC_BulletLife_GD : MonoBehaviour
 {
     public int LifeTime;
     public int RotationSpeed;
+    public float minmum = 0.0f;
+    public float maximum = 90.0f;
 
     void Start()
     {
         StartCoroutine("Countdown", LifeTime);
     }
 
-    private void Update()
+    /*private void Update()
     {
         transform.Rotate(0, RotationSpeed * Time.deltaTime, 0);
-
-    }
+        if (transform.rotation.y <= maximum)
+        {
+            RotationSpeed = RotationSpeed * - 1;
+        }
+    }*/
 
     private IEnumerator Countdown(int time)
     {
         while (time > 0)
         {
-            Debug.Log(time--);
+            time--;
             yield return new WaitForSeconds(1);
         }
-        Debug.Log("Countdown Complete!");
-        Destroy(gameObject);
+                Destroy(gameObject);
     }
 
 }
