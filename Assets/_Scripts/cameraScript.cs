30e07793226a2edafb9156c0a4927d9bbd3e3ce8
Samuel Bourassa <samuel.i.bourassa@gmail.com>
2017-11-11 19:07:08 -0500

    Post Processing

--- Assets/_Scripts/cameraScript.cs
+++ Assets/_Scripts/cameraScript.cs
@@ -7,28 +7,25 @@ public class cameraScript : MonoBehaviour {
 
 	bool oSequenceGoing = true;
 	Camera cam;
-	public float openingSequenceDuration;
+	public float openingSequenceDuration, waitTime;
 
 	// Use this for initialization
 	void Start () {
 		cam = Camera.main;
+		StartCoroutine("OpeningSequence");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKeyDown(KeyCode.Space)) {
-			StartCoroutine("OpeningSequence");
-			while(oSequenceGoing == true) {
-				return;
-			}
-		}
 	}
 
 	IEnumerator OpeningSequence() {
+		yield return new WaitForSeconds(waitTime);
 		Tween rotation = transform.DORotate(Vector2.zero, openingSequenceDuration).SetEase(Ease.InSine);
 		Tween translation = transform.DOMove(Vector2.zero, openingSequenceDuration).SetEase(Ease.InSine);
 		oSequenceGoing = false;
 		yield return new WaitForEndOfFrame();
+		//Start GAME
 	}
 }
